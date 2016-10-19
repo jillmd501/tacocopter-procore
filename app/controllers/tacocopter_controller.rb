5dd060cd1203d3e86596e59417a5120bb8f560ab
Jill Donohue <jillmd501@gmail.com>
2016-10-19 15:36:18 -0600

    Remove salsa information for heroku

--- app/controllers/tacocopter_controller.rb
+++ app/controllers/tacocopter_controller.rb
@@ -10,12 +10,12 @@ class TacocopterController < ApplicationController
 
 # List only restaurants with params selected by user	
   def search
-	  params[:salsas].nil? && params[:tacos].nil?
+	if params[:salsas].nil? && params[:tacos].nil?
 	  flash[:notice] = "Please select tacos or salsas!"
 	if
 	  @stores_with_tacos = Store.find_stores_with_selected_tacos(params[:tacos])
-	  @stores_with_salsas= Store.find_stores_with_selected_salsas(params[:salsas])
-	  @final_stores = @stores_with_tacos + @stores_with_salsas
+	  # @stores_with_salsas= Store.find_stores_with_selected_salsas(params[:salsas])
+	  @final_stores = @stores_with_tacos
     end
   end
 
--- app/models/store.rb
+++ app/models/store.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Store < ActiveRecord::Base
   belongs_to :city
   has_and_belongs_to_many :tacos
-  has_and_belongs_to_many :salsas
+  # has_and_belongs_to_many :salsas
 
   validates :name, presence: true
   validates :zagat_rating, presence: true
@@ -19,12 +19,12 @@ class Store < ActiveRecord::Base
   #   binding.pry
   # end
 
-  def self.find_stores_with_selected_salsas(salsas)
-    if salsas.include?('salsas')
-      includes(:salsas).all
-    else
-      includes(:salsas).where(salsas: { name: salsas })
-    end
-  end
+  # def self.find_stores_with_selected_salsas(salsas)
+  #   if salsas.include?('salsas')
+  #     includes(:salsas).all
+  #   else
+  #     includes(:salsas).where(salsas: { name: salsas })
+  #   end
+  # end
 
 end
