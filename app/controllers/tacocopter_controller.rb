32b1ee3194acafdb48fd4ff640c43c0192c6f8c6
Jill Donohue <jillmd501@gmail.com>
2016-10-19 15:40:51 -0600

    heroku fix

--- app/controllers/tacocopter_controller.rb
+++ app/controllers/tacocopter_controller.rb
@@ -16,7 +16,5 @@ class TacocopterController < ApplicationController
 	  @stores_with_tacos = Store.find_stores_with_selected_tacos(params[:tacos])
 	  # @stores_with_salsas= Store.find_stores_with_selected_salsas(params[:salsas])
 	  @final_stores = @stores_with_tacos
-    end
   end
-
 end
