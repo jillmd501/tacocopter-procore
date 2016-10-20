6e6a8a77225d1693cb8bfec04b5ef0ada2b71a52
Jill Donohue <jillmd501@gmail.com>
2016-10-20 12:25:39 -0600

    Update README with heroku link

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Here in Santa Barbara, we're passionate about our tacos. In fact, we get tacos c
 
 Your mission, should you choose to accept it, is to help us by writing a Rails app that solves this conundrum.
 
+Check it out all deployed and whatnot! [tacocopter-link](https://jill-tacocopter.herokuapp.com/ "Jill's Tacocopter")
+
 ![taco image](https://s-media-cache-ak0.pinimg.com/originals/99/5b/e0/995be049ba2873da650c5242aca532db.jpg)
--- app/controllers/tacocopter_controller.rb
+++ app/controllers/tacocopter_controller.rb
@@ -1,17 +1,22 @@
 require 'pry'
 
 class TacocopterController < ApplicationController
-	  def index
+  def index
     @tacos = Taco.all
     @salsas = Salsa.all
   end
 
   def search
+	@stores_with_tacos = Store.find_stores_with_selected_tacos(params[:tacos])
+	@stores_with_salsas= Store.find_stores_with_selected_salsas(params[:salsas])
+	
 	if params[:salsas].nil? && params[:tacos].nil?
-	  # redirect_to index_path
+		binding.pry
+	  flash[:notice]= "Pick tacos or salsas!"
+	elsif @stores_with_salsas.empty? || @stores_with_tacos.empty?
+      @final_stores = (@stores_with_salsas + @stores_with_tacos)
+    else
+	  @final_stores = @stores_with_tacos & @stores_with_salsas
 	end
-	  @stores_with_tacos = Store.find_stores_with_selected_tacos(params[:tacos])
-	  @stores_with_salsas= Store.find_stores_with_selected_salsas(params[:salsas])
-	  @final_stores = @stores_with_tacos
   end
 end
